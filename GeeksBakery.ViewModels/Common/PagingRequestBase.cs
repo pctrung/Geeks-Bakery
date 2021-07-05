ecbad2462659c5cb4934901d96b85d90c601d63e
Phạm Chí Trung <44517184+TrungXiIT@users.noreply.github.com>
2021-07-05 22:24:14 +0700

    create view model project

---
+++ GeeksBakery.ViewModels/Common/PagingRequestBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeeksBakery.ViewModels.Common
+{
+    public class PagingRequestBase
+    {
+        public int PageSize { get; set; }
+        public int PageIndex { get; set; }
+    }
+}
