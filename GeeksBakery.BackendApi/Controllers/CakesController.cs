c5756d2db776f73c21ae80e651069fca8e0af377
Phạm Chí Trung <44517184+TrungXiIT@users.noreply.github.com>
2021-07-12 00:45:05 +0700

    Update CakesController.cs

--- GeeksBakery.BackendApi/Controllers/CakesController.cs
+++ GeeksBakery.BackendApi/Controllers/CakesController.cs
@@ -1,8 +1,11 @@
 ﻿using GeeksBakery.Application.Interfaces;
 using GeeksBakery.ViewModels.Requests.Cake;
+using GeeksBakery.ViewModels.Requests.Review;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace GeeksBakery.BackendApi.Controllers
@@ -45,21 +48,6 @@ namespace GeeksBakery.BackendApi.Controllers
                     return NotFound($"Cannot find a cake with Id: {cakeId}");
                 }
                 return Ok(JsonConvert.SerializeObject(data));
-            }
-            catch (Exception e)
-            {
-                return BadRequest(e.Message);
-            }
-        }
-
-        [HttpGet("{cakeId}/reviews")]
-        public async Task<IActionResult> GetReviews(int cakeId)
-        {
-            try
-            {
-                var reviews = await _reviewService.GetByCakeIdAsync(cakeId);
-
-                return Ok(JsonConvert.SerializeObject(reviews));
             }
             catch (Exception e)
             {
@@ -136,6 +124,84 @@ namespace GeeksBakery.BackendApi.Controllers
                 if (result == 0)
                 {
                     return NotFound($"Cannot find a cake with Id: {cakeId}");
+                }
+
+                return Ok(result);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpGet("{cakeId}/reviews")]
+        public async Task<IActionResult> GetReviews(int cakeId)
+        {
+            try
+            {
+                var reviews = await _reviewService.GetByCakeIdAsync(cakeId);
+
+                return Ok(JsonConvert.SerializeObject(reviews));
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpGet("{cakeId}/reviews/{reviewId}")]
+        public async Task<IActionResult> GetReviewById(int reviewId)
+        {
+            try
+            {
+                var review = await _reviewService.GetByIdAsync(reviewId);
+
+                return Ok(JsonConvert.SerializeObject(review));
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpPost("{cakeId}/reviews")]
+        public async Task<IActionResult> CreateReview([FromForm] ReviewCreateRequest request)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+                var reviewId = await _reviewService.CreateAsync(request);
+
+                if (reviewId == 0)
+                {
+                    return BadRequest();
+                }
+
+                var data = await _reviewService.GetByIdAsync(reviewId);
+                if (data == null)
+                {
+                    return NotFound($"Cannot find a review with Id: {reviewId}");
+                }
+                return Ok(JsonConvert.SerializeObject(data));
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpDelete("{cakeId}/reviews/{reviewId}")]
+        public async Task<IActionResult> DeleteReview(int reviewId)
+        {
+            try
+            {
+                var result = await _reviewService.DeleteAsync(reviewId);
+                if (result == 0)
+                {
+                    return NotFound($"Cannot find a review with Id: {reviewId}");
                 }
 
                 return Ok(result);
