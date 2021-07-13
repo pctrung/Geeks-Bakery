67fe42a92b8ed5edf38f67f5cd7f584e282a15a0
Phạm Chí Trung <44517184+TrungXiIT@users.noreply.github.com>
2021-07-13 19:22:56 +0700

    Feat: Initial login page

--- GeeksBakery/wwwroot/js/app.js
+++ GeeksBakery/wwwroot/js/app.js
@@ -2,11 +2,6 @@ $(document).ready(function () {
   screenLoaded(2000);
 });
 
-// function loadingScreen(){
-//   var loadingScreen = document.createElement("div").id("loading-screen");
-//   document.body.appendChild(loadingScreen);
-// }
-
 function screenLoaded(timeOut) {
   var loadingScreen = $("#loading-screen");
   loadingScreen.addClass("animation-loaded");
